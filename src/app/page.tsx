290383cabf7d474b90a9d7b7c6383926e79f9754
Kevin <koolkev246@gmail.com>
2023-08-19 22:41:12 -0400

    progress to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,144 @@
-import Image from 'next/image'
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col min-h-screen">
+      <nav className="flex items-center justify-between bg-gray-800 text-white p-4">
+        <div className="flex items-center justify-center">
+          <Link legacyBehavior href="/">
+            <a className="flex items-center justify-center">
+              <Image
+                src="/logo.png"
+                alt="Logo"
+                width={40}
+                height={40}
+                className="mr-2"
+              />
+              <span className="font-bold text-lg ml-3">
+                Kevin Kwan's Portfolio
+              </span>
+            </a>
+          </Link>
+        </div>
+        <div className="flex items-center justify-center md:hidden">
+          <button
+            className="text-white hover:text-gray-300 focus:outline-none"
+            onClick={toggleMenu}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMenuOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M19.2929 6.29289C19.6834 5.90237 20.3166 5.90237 20.7071 6.29289C21.0976 6.68342 21.0976 7.31658 20.7071 7.70711L13.4142 15L20.7071 22.2929C21.0976 22.6834 21.0976 23.3166 20.7071 23.7071C20.3166 24.0976 19.6834 24.0976 19.2929 23.7071L12 16.4142L4.70711 23.7071C4.31658 24.0976 3.68342 24.0976 3.29289 23.7071C2.90237 23.3166 2.90237 22.6834 3.29289 22.2929L10.5858 15L3.29289 7.70711C2.90237 7.31658 2.90237 6.68342 3.29289 6.29289C3.68342 5.90237 4.31658 5.90237 4.70711 6.29289L12 13.5858L19.2929 6.29289Z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6C3.44772 6 3 6.44772 3 7C3 7.55228 3.44772 8 4 8H20C20.5523 8 21 7.55228 21 7C21 6.44772 20.5523 6 20 6H4ZM3 12C3 11.4477 3.44772 11 4 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H4C3.44772 13 3 12.5523 3 12ZM20 16H4C3.44772 16 3 16.4477 3 17C3 17.5523 3.44772 18 4 18H20C20.5523 18 21 17.5523 21 17C21 16.4477 20.5523 16 20 16Z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        <div className="hidden md:flex items-center justify-center">
+          {' '}
+          {/* Added justify-center class */}
+          <Link legacyBehavior href="/">
+            <a className="mx-4 hover:text-gray-300">Home</a>
+          </Link>
+          <Link legacyBehavior href="/about">
+            <a className="mx-4 hover:text-gray-300">About Me</a>
+          </Link>
+          <Link legacyBehavior href="/experience">
+            <a className="mx-4 hover:text-gray-300">My Experience</a>
+          </Link>
+          <Link legacyBehavior href="/projects">
+            <a className="mx-4 hover:text-gray-300">My Projects</a>
+          </Link>
+          <Link legacyBehavior href="/contact">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Contact Me
+            </button>
+          </Link>
+        </div>
+      </nav>
+      <div
+        className={`${
+          isMenuOpen ? 'block' : 'hidden'
+        } fixed bg-gray-800 text-white w-full flex justify-center text-center md:hidden mt-16`}
+      >
+        <div className="max-w-2xl mx-auto p-4">
+          <Link legacyBehavior href="/">
+            <a
+              className="block py-2 px-4 hover:bg-gray-700"
+              onClick={toggleMenu}
+            >
+              Home
+            </a>
+          </Link>
+          <Link legacyBehavior href="/about">
+            <a
+              className="block py-2 px-4 hover:bg-gray-700"
+              onClick={toggleMenu}
+            >
+              About Me
+            </a>
+          </Link>
+          <Link legacyBehavior href="/experience">
+            <a
+              className="block py-2 px-4 hover:bg-gray-700"
+              onClick={toggleMenu}
+            >
+              My Experience
+            </a>
+          </Link>
+          <Link legacyBehavior href="/projects">
+            <a
+              className="block py-2 px-4 hover:bg-gray-700"
+              onClick={toggleMenu}
+            >
+              My Projects
+            </a>
+          </Link>
+          <Link legacyBehavior href="/contact">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Contact Me
+            </button>
+          </Link>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <main className="flex-1 p-4">
+        <div className="max-w-2xl mx-auto">
+          <p className="text-2xl font-bold mb-4">Welcome to my portfolio!</p>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tincidunt, lorem vel bibendum bibendum, elit sapien bibendum sapien,
+            vel bibendum sapien sapien vel sapien.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </div>
+      </main>
+      <footer className="bg-gray-800 text-white p-4">
+        <div className="max-w-2xl mx-auto">
+          <p className="text-center">
+            &copy; 2023 - Kevin Kwan. All Rights Reserved.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+      </footer>
+    </div>
+  );
 }
