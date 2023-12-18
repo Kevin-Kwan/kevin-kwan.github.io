36f6b658f74a00aaca415a237de8efccc9f3e773
Kevin <koolkev246@gmail.com>
2023-12-17 22:32:06 -0500

    Removed "Home" from Navbar Menu

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -14,11 +14,11 @@ const NavbarMenu = ({ isMenuOpen, toggleMenu }: MenuProps) => {
       } fixed bg-gray-800 text-white w-full flex justify-center text-center md:hidden mt-16`}
     >
       <div className="max-w-2xl mx-auto p-4">
-        <Link legacyBehavior href="/">
+        {/* <Link legacyBehavior href="/">
           <a className="block py-2 px-4 hover:bg-gray-700" onClick={toggleMenu}>
             Home
           </a>
-        </Link>
+        </Link> */}
         <Link legacyBehavior href="/about">
           <a className="block py-2 px-4 hover:bg-gray-700" onClick={toggleMenu}>
             About Me
