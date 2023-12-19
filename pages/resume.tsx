16970ddd0a4995e8213064167c4347d753278021
Kevin <koolkev246@gmail.com>
2023-12-19 12:52:02 -0500

    Update resume page to display PDF with download link using object instead of embed

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -9,11 +9,19 @@ export default function Resume() {
         <title>Kevin Kwan | Résumé</title>
       </Head>
       <main className="flex-1 p-4" style={{ height: '100vh' }}>
-        <embed
-          src={process.env.NEXT_PUBLIC_RESUME_LINK}
+        <object
+          data={process.env.NEXT_PUBLIC_RESUME_LINK}
           type="application/pdf"
           style={{ width: '100%', height: '100%' }}
-        />
+        >
+          <p>
+            It appears you don't have a PDF plugin for this browser. No
+            problem... you can{' '}
+            <a href={process.env.NEXT_PUBLIC_RESUME_LINK}>
+              click here to download the PDF file.
+            </a>
+          </p>
+        </object>
       </main>
     </Layout>
   );
