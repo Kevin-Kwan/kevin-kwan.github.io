8ce03c465c9d6d2cc26f449b1fce61860b34271f
Kevin <koolkev246@gmail.com>
2023-12-19 13:02:23 -0500

    Attempt to use Adobe DC again

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import Layout from '../components/RootLayout';
+declare const AdobeDC: any;
 
 export default function Resume() {
+  useEffect(() => {
+    // Check if the script has already been loaded
+    if (
+      document.querySelector(
+        'script[src="https://acrobatservices.adobe.com/view-sdk/viewer.js"]'
+      )
+    ) {
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.src = 'https://acrobatservices.adobe.com/view-sdk/viewer.js';
+    script.onload = () => {
+      document.addEventListener('adobe_dc_view_sdk.ready', function () {
+        var adobeDCView = new AdobeDC.View({
+          clientId: process.env.NEXT_PUBLIC_ADOBE_CLIENT_ID,
+          divId: 'adobe-dc-view',
+        });
+        adobeDCView.previewFile(
+          {
+            content: {
+              location: {
+                url: process.env.NEXT_PUBLIC_RESUME_LINK,
+              },
+            },
+            metaData: { fileName: 'Kevin_Kwan_Resume_2024.pdf' },
+          },
+          {
+            embedMode: 'FULL_WINDOW',
+            defaultViewMode: 'FIT_PAGE',
+            showAnnotationTools: true,
+            showDownloadPDF: true,
+          }
+        );
+      });
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <Layout>
       <Head>
         <title>Kevin Kwan | Résumé</title>
       </Head>
       <main className="flex-1 p-4" style={{ height: '100vh' }}>
-        <object
-          data={process.env.NEXT_PUBLIC_RESUME_LINK}
-          type="application/pdf"
-          style={{ width: '100%', height: '100%' }}
-        >
-          <p>
-            It appears you don't have a PDF plugin for this browser. No
-            problem... you can{' '}
-            <a href={process.env.NEXT_PUBLIC_RESUME_LINK}>
-              click here to download the PDF file.
-            </a>
-          </p>
-        </object>
+        <div id="adobe-dc-view"></div>
       </main>
     </Layout>
   );
