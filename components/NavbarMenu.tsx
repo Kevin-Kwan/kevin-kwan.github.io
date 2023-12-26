e94739f6d0dad513b0935dd375e04c688d5dd8f7
Kevin Kwan <kkwan9@gatech.edu>
2023-12-26 01:30:20 -0500

    Update NavbarMenu.tsx

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -32,7 +32,7 @@ const NavbarMenu = ({ isMenuOpen, toggleMenu }: MenuProps) => {
             About Me
           </button>
         </Link> */}
-        <Link legacyBehavior href="/resume">
+        <Link legacyBehavior href={process.env.NEXT_PUBLIC_RESUME_LINK}>
           <button
             type="button"
             className="text-lg md:text-xl w-full text-white block py-2 px-4 hover:bg-gray-700 duration-200 hover:text-blue-500"
